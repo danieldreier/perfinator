87c7dd64d42da46bf451ddd37623b2204cf443b8
Daniel Dreier <d@puppetlabs.com>
2015-01-24 00:38:24 -0800

    Add customizable statd namespace prefixing

--- lib/perfinator/loadtest.rb
+++ lib/perfinator/loadtest.rb
@@ -7,13 +7,14 @@ module Perfinator
     include Methadone::CLILogging
 
     def self.run(statsd_server: 'localhost', statsd_port: 8125, concurrent: 10,
-                requests: 500, delay: 2, url_file: Nil)
+                requests: 500, delay: 2, statsd_namespace: 'perfinator', url_file: Nil)
       metrics_list = ['connect_time', 'start_transfer_time', 'name_lookup_time',
                       'pre_transfer_time', 'redirect_time', 'start_transfer_time',
                       'total_time', 'downloaded_bytes', 'download_speed']
 
       Parallel.map(1..requests, :in_threads => concurrent, :progress => "Load Testing") {
         statsd = Statsd.new statsd_server, statsd_port
+        statsd.namespace = statsd_namespace
         metrics = Hash.new
 
         begin
