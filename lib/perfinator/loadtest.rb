bbd830e025813de3c33040a3bc4c646b3ec29feb
Daniel Dreier <d@puppetlabs.com>
2015-01-24 00:22:18 -0800

    Improve logging / error messages

--- lib/perfinator/loadtest.rb
+++ lib/perfinator/loadtest.rb
@@ -4,6 +4,7 @@ module Perfinator
     require 'statsd-ruby'
     require 'parallel'
     require 'ruby-progressbar'
+    include Methadone::CLILogging
 
     def self.run(statsd_server: 'localhost', statsd_port: 8125, concurrent: 10,
                 requests: 500, delay: 2, url_file: Nil)
@@ -17,10 +18,11 @@ module Perfinator
 
         begin
           url = File.readlines(url_file).sample.chomp
+          info "requesting: #{url}"
           request = Curl::Easy.new(url)
           request.get
         rescue Curl::Err::HostResolutionError
-          puts 'cannot parse url: ' + url.to_s
+          logger.error('cannot parse url: ' + url.to_s)
           break
         end
         statsd.increment 'requests'
@@ -28,9 +30,8 @@ module Perfinator
         for metric in metrics_list
           metrics[:"#{metric}"] = request.send(metric)
           statsd.timing metric, metrics[:"#{metric}"]
-      #    puts metric + ": " + metrics[:"#{metric}"].to_s
+          debug "sent statsd metric: '#{metric}' with value " + metrics[:"#{metric}"].to_s
         end
-      #  puts "total_time: " + metrics[:total_time].to_s
       }
     end
   end
